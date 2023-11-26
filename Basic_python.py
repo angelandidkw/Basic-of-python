719c1f96802cff8980d770f2eb0de641e681e9fe
angelandidkw <148724145+angelandidkw@users.noreply.github.com>
2023-11-26 13:14:34 -0600

    Add files via upload

--- Basic_python.py
+++ Basic_python.py
@@ -1,7 +1,12 @@
+
+
+# SUN NOV 26 1:00
+
 # Hello, I will help you understand basic python (It really basic)
 
-# We will understand print() # Example 1, input() #Example 1 and #Example 2, list = , if, elif and else and lastly using equtions to in python 
+# We will understand print() # Example 1, input() #Example 1 and #Example 2, list = , if, elif and else 
 
+# CREATED BY angelandidkw ((GITHUB)
 #----------------------------------------------------------------------------------------------------------------------#
 #Print the basic
 
@@ -19,10 +24,10 @@
 # Quiz 
 
 # I want you print your name and why you are learning python!
-
+# CREATED BY angelandidkw (GITHUB)
 
 #----------------------------------------------------------------------------------------------------------------------#
-
+# CREATED BY angelandidkw ( GITHUB1 )
 
 # User input
 
@@ -34,7 +39,7 @@
 
 # Example 1
 
-# I will be using if and else statement if you don't understand go to if and elif statment or go to example 2
+# I will be using if and else statement if you don't understand go to if and elif statment or go to example 2   
 
 # question = input("Would you like to play?")
 
@@ -63,12 +68,13 @@
 # Login works
 # Password:
 # password works
-
+# CREATED BY angelandidkw ( GITHUB )
 
 
 #----------------------------------------------------------------------------------------------------------------------#
 
 # Creating list 
+
 # choose_a_veriable_of_your_choice = ("Mark", "Zack", "Miranda", "Zack", "Mary")
 # choose_a_veriable_of_your_choice_1 = ["Mark", "Zack", "Miranda", "Zack", "Mary"]
  
@@ -76,6 +82,74 @@
 # print(choose_a_veriable_of_your_choice_1) # Output: ['Mark', 'Zack', 'Miranda', 'Zack', 'Mary']
 #  first we are choosing any veriable then making list using () or [] then we are printing veriable because it reference the list
 
-# x = ("Mark", "Zack", "Miranda", "Zack", "Mary")
+# Example 1
+# We are going call a veriable like mark
+#x = ("Mark", "Zack", "Miranda", "Zack", "Mary")
 
-# print(x[0])
+#print(x[0]) # The reason we 0 because in list has numbers 
+                                    #0       #1     #2        #3     #4
+                                    # "Mark" "Zack" "Miranda" "Zack" "Mary"
+
+
+#List of Numbers:
+#numbers = [1, 2, 3, 4, 5]
+
+#List of Strings:
+
+#fruits = ["apple", "banana", "orange", "grape"]
+#List of Mixed Types:
+
+
+#mixed_list = [1, "hello", 3.14, True]
+
+#List of Lists (Nested Lists):
+#nested_list = [[1, 2, 3], ["a", "b", "c"], [True, False]]
+
+
+#List of Tuples:
+#coordinates = [(1, 2), (3, 4), (5, 6)]
+#
+#List of Boolean Values:
+#boolean_values = [True, False, True, True, False]
+#
+#List of Dictionaries:
+#students = [
+#    {"name": "Alice", "age": 25, "grade": "A"},
+#    {"name": "Bob", "age": 22, "grade": "B"},
+#    {"name": "Charlie", "age": 24, "grade": "A"}
+#]
+
+
+#Empty List:
+#empty_list = []
+
+
+# There is whole lot what you can do with list but I can't through all of type list :)
+# CREATED BY angelandidkw ( GITHUB )
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+# if,elif,else keywords 
+
+    # Code to be executed if condition1 is true
+#if condition1:
+#elif condition2:
+    # Code to be executed if condition2 is true
+#else:
+    # Code to be executed if none of the conditions are true
+
+#username = input("Enter username: ")
+
+#if username == "mark": # first going see username what did user input now if username is true or same as "mark" it is going print Welcome mark
+    # print("Welcome mark") 
+
+#elif username == "Zack": # If don't match mark but if it did match Zack going to say print Welcome Zach
+    # print("Welcome Zach")
+#else:
+   # print("Welcome!") # Lastly if confition is not true it is going here else going print "Welcome "
+
+# There lot more that you could with if, elif, else but I'm showing the basic and general basic 
+
+# Thank you 
+# CREATED BY angelandidkw
+# GITHUB ACC: angelandidkw
