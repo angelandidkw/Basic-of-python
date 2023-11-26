f6029b12fa53ccfdca42ff91561b75cdea3fa9e2
angelandidkw <148724145+angelandidkw@users.noreply.github.com>
2023-11-26 13:02:24 -0600

    Add files via upload

---
+++ Basic_python.py
@@ -0,0 +1,81 @@
+# Hello, I will help you understand basic python (It really basic)
+
+# We will understand print() # Example 1, input() #Example 1 and #Example 2, list = , if, elif and else and lastly using equtions to in python 
+
+#----------------------------------------------------------------------------------------------------------------------#
+#Print the basic
+
+# print("Hello world") 
+# Output should be - Hello world - 
+# One thing is important is don't forget to put "" 
+# IF you don't active the function using ""  **It will show a error**
+
+
+#Example 1 
+
+# print("Hello, Coding class!") # OUTPUT - Hello, Coding class
+# print("My name is ******") # OUTPUT - My name is ******
+
+# Quiz 
+
+# I want you print your name and why you are learning python!
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+
+# User input
+
+# ver is veriable using = to assign the ver and it reference input 
+# ver = input("Hello, What is your name?") # OUTPUT - Hello, What is your name? []  <-- user input
+# THIS IS THE CODE ^^^ you can delete the hash to active the function
+
+# user_input = input("This is like print functions")
+
+# Example 1
+
+# I will be using if and else statement if you don't understand go to if and elif statment or go to example 2
+
+# question = input("Would you like to play?")
+
+# THIS IS THE CODE ^^^ you can delete the hash to active the function
+
+# Im making if statment to check the statement 
+
+# if question == "yes" or question == "Yes":
+    # print("Alright") # if user said Yes or yes it will print "alright"
+# else:
+    # print("Alright I have nice day!") # else going print "Alright I have nice day!"
+
+
+#Example 2 
+
+# user = input("Usermame ===> ") # Asking a user put username 
+# print("Login works!") # print the function after function is done asking the input 2
+
+
+# Quiz time!
+
+# I want you create two input one username and one password and after username print Login works 
+# After password print password works
+
+# output Username: 
+# Login works
+# Password:
+# password works
+
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+# Creating list 
+# choose_a_veriable_of_your_choice = ("Mark", "Zack", "Miranda", "Zack", "Mary")
+# choose_a_veriable_of_your_choice_1 = ["Mark", "Zack", "Miranda", "Zack", "Mary"]
+ 
+# print(choose_a_veriable_of_your_choice) # Output: ('Mark', 'Zack', 'Miranda', 'Zack', 'Mary')
+# print(choose_a_veriable_of_your_choice_1) # Output: ['Mark', 'Zack', 'Miranda', 'Zack', 'Mary']
+#  first we are choosing any veriable then making list using () or [] then we are printing veriable because it reference the list
+
+x = ("Mark", "Zack", "Miranda", "Zack", "Mary")
+
+print(x[0])
